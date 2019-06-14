46b05f14ffb59ffa24c6ee5cf29bea66f9ae115c
Jyoti Das <bootcampatut@gmail.com>
2019-06-13 23:47:04 -0500

    moddified the enter time text field.

--- assets/firebase/trainapp.js
+++ assets/firebase/trainapp.js
@@ -65,47 +65,60 @@ database.ref().on("child_added", function(snapshot) {
       
    
 console.log("starttime:"+traintime);
-var startTime = sv.traintime;
-var startMinutes = parseInt(startTime.split(":")[1])
-var startHours = parseInt(startTime.split(":")[0])
-console.log(startTime)
-console.log(startMinutes)
-console.log(startHours)
-var totalStartMinutes = startHours * 60 + startMinutes;
-console.log(totalStartMinutes)
-var roundTime =sv.frequency;
+// var startTime = sv.traintime;
+// var startMinutes = parseInt(startTime.split(":")[1])
+// var startHours = parseInt(startTime.split(":")[0])
+// console.log(startTime)
+// console.log(startMinutes)
+// console.log(startHours)
+// var totalStartMinutes = startHours * 60 + startMinutes;
+// console.log(totalStartMinutes)
+// var roundTime =sv.frequency;
 
-var currentTime =moment().format('hh:mm A')
-var currentMinutes = parseInt(currentTime.split(":")[1])
-var currentHours = parseInt(currentTime.split(":")[0])
-console.log(currentTime)
-console.log(currentMinutes)
-console.log(currentHours)
-var totalCurrentMinutes = currentMinutes *60 +currentHours ;
-console.log(totalCurrentMinutes)
+// var currentTime =moment().format('hh:mm A')
+// var currentMinutes = parseInt(currentTime.split(":")[1])
+// var currentHours = parseInt(currentTime.split(":")[0])
+// console.log(currentTime)
+// console.log(currentMinutes)
+// console.log(currentHours)
+// var totalCurrentMinutes = currentMinutes *60 +currentHours ;
+// console.log(totalCurrentMinutes)
 
-var totalDifference = totalCurrentMinutes - totalStartMinutes;
-console.log("Total Minutes" + totalDifference)
+// var totalDifference = totalCurrentMinutes - totalStartMinutes;
+// console.log("Total Minutes" + totalDifference)
 
-var trainAway = (roundTime - totalDifference%roundTime)
-console.log(trainAway + " minutes away")
+// var trainAway = (roundTime - totalDifference%roundTime)
+// console.log(trainAway + " minutes away")
 
-var arrivalTimeMinutes = totalCurrentMinutes + trainAway
-console.log(arrivalTimeMinutes)
-var arrivalMinutePlace = arrivalTimeMinutes % 60
-console.log(arrivalMinutePlace)
-var arrivalHourPlace = (arrivalTimeMinutes - arrivalMinutePlace)/60
-console.log(arrivalHourPlace)
-var trainArrival=arrivalHourPlace + ":" + arrivalMinutePlace;
-// var train=moment(trainArrival, 'hh:mm').format('hh:mm A');
-console.log(arrivalHourPlace + ":" + arrivalMinutePlace);
-// console.log(train);
+// var arrivalTimeMinutes = totalCurrentMinutes + trainAway
+// console.log(arrivalTimeMinutes)
+// var arrivalMinutePlace = arrivalTimeMinutes % 60
+// console.log(arrivalMinutePlace)
+// var arrivalHourPlace = (arrivalTimeMinutes - arrivalMinutePlace)/60
+// console.log(arrivalHourPlace)
+// var trainArrival=arrivalHourPlace + ":" + arrivalMinutePlace;
+// // var train=moment(trainArrival, 'hh:mm').format('hh:mm A');
+// console.log(arrivalHourPlace + ":" + arrivalMinutePlace);
+// // console.log(train);
+        //var nextArr;
+        var minAway;
+        // Chang year so first train comes before now
+        var firstTrainNew = moment(sv.traintime, "hh:mm").subtract(1, "years");
+        // Difference between the current and firstTrain
+        var diffTime = moment().diff(moment(firstTrainNew), "minutes");
+        var remainder = diffTime % sv.frequency;
+        // Minutes until next train
+        var minAway = sv.frequency - remainder;
+        // Next train time
+        var nextTrain = moment().add(minAway, "minutes");
+        nextTrain = moment(nextTrain).format("HH:mm A");
 
-{/* <td>"+ traintime +"</td> */}
+
+
   
 
 
-    $("table").append("<tr><td>" + trainname + "</td><td>" + desti +"</td><td>" +  frequency + "</td><td>" +trainArrival+ "</td><td>" + trainAway + "</td><tr>");
+    $("table").append("<tr><td>" + trainname + "</td><td>" + desti +"</td><td>" +  frequency + "</td><td>" +nextTrain+ "</td><td>" + minAway + "</td><tr>");
 
 
     
